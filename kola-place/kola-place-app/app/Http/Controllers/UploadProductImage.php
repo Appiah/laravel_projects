2c2b1091c6071774711de5c0cc204ad6d02d477b
Appiah <emap218@gmail.com>
2024-05-11 11:07:20 +0000

    Just a few fundamental validations for the upcoming requests : uploading of a product image

--- kola-place/kola-place-app/app/Http/Controllers/UploadProductImage.php
+++ kola-place/kola-place-app/app/Http/Controllers/UploadProductImage.php
@@ -14,12 +14,19 @@ class UploadProductImage extends Controller{
 
         $incomingFields = $request->all();
 
-        $productId = $incomingFields['productId'];//NOT SET BY THE USER, it is fetched by extension from the product created by the merchant
+        //Remember to put variables here to check for the presence of the required fields
+        //And use these variables to send feedback to the user and also to process the upcoming request
 
-        $imagePathType = $incomingFields['imagePathType'];//MANDATORY : and it would be fetched based on the path type specified by the user [ LOCAL_FOLDER => 0, ONLINE_PATH => 1 ]
-        $imagePath = $incomingFields['imagePath'];//MANDATORY : and it would be fetched based on the path type
-        
-        $imageDescriptionLocale = $incomingFields['imageDescriptionLocale'];//default : "en";
+        if($request->has('product_id')){
+            $productId = $incomingFields['productId'];//NOT SET BY THE USER, it is fetched by extension from the product created by the merchant
+        }
+
+        if($request->has('imagePathType') && $request->has('imagePath')){
+            $imagePathType = $incomingFields['imagePathType'];//MANDATORY : and it would be fetched based on the path type specified by the user [ LOCAL_FOLDER => 0, ONLINE_PATH => 1 ]
+            $imagePath = $incomingFields['imagePath'];//MANDATORY : and it would be fetched based on the path type
+        }
+
+        $imageDescriptionLocale = $incomingFields['imageDescriptionLocale'];//OPTIONAL : default : "en";
         $imageDescription = $incomingFields['imageDescription'];//OPTIONAL : should not be more than 100 characters
         $imageAltContent = $incomingFields['imageAltContent'];//OPTIONAL : should not be more than 20 characters
         
